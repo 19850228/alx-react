4f0541f00decfef3b8eac51788694bf8b7027071
19850228 <fmthombothi@yahoo.com>
2024-07-22 12:38:53 +0000

    webpack

---
+++ task_1/public/bundle.js
@@ -0,0 +1,2 @@
+/*! For license information please see bundle.js.LICENSE.txt */
+(()=>{var t={755:function(t,e){var n;!function(e,n){"use strict";"object"==typeof t.exports?t.exports=e.document?n(e,!0):function(t){if(!t.document)throw new Error("jQuery requires a window with a document");return n(t)}:n(e)}("undefined"!=typeof window?window:this,(function(r,i)
